f2b53f0dfb0cf097400661ff5e6c1ca120044d43
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-02 16:19:00 +0300

    Добавил Базовый класс ForestObject

---
+++ src/forest/ForestObject.java
@@ -0,0 +1,16 @@
+package forest;
+
+public class ForestObject {
+
+    private static int total_ids = 1;
+
+    protected int id;
+    protected String name;
+    protected float mass;
+
+    public ForestObject(String name, float mass) {
+        this.name = name;
+        this.mass = mass;
+        this.id = total_ids++;
+    }
+}
