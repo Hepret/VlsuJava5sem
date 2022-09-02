137b6d52127fa365d46712279682af0388a94f6f
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-02 16:36:17 +0300

    Добавил класс травы Grass.java

---
+++ src/forest/plants/Grass.java
@@ -0,0 +1,10 @@
+package forest.plants;
+
+import forest.Food;
+import forest.ForestObject;
+
+public class Grass extends ForestObject implements Food {
+    public Grass(String name, float mass) {
+        super(name, mass);
+    }
+}
